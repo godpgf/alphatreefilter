ac1049fa52cb02ac9fb6bd90192238df2e167984
yu.yan <yu.yan@pingxx.com>
2017-06-15 14:13:16 +0800

    continue

--- demo.py
+++ demo.py
@@ -4,7 +4,9 @@ from alphatreefilter import *
 import datetime
 from stdb import *
 import pandas as pd
+import numpy as np
 from alphatree import write_alpha_tree_list
+from portfolio import MaxSharpePortfolio
 
 if __name__ == '__main__':
     #1、读出所有alphatree
@@ -23,13 +25,56 @@ if __name__ == '__main__':
     #4、存档选择的alphatree
     write_alpha_tree_list('doc/alpha_tree_%s.txt'%get_dt(datetime.datetime.now()), [alphatree_score.alphatree for alphatree_score in alphatree_score_list])
 
+    #if len(alphatree_score_list) > 0:
+        #存档选择的stock
+    #    pd.DataFrame({
+    #        'stock': [','.join(codes) for codes in [alphatree_score.codes for alphatree_score in alphatree_score_list]],
+    #        'score': [alphatree_score.score for alphatree_score in alphatree_score_list],
+    #        'sharp': [alphatree_score.score / alphatree_score.score_stddev for alphatree_score in alphatree_score_list],
+    #        'day': [alphatree_score.hold_day_num for alphatree_score in alphatree_score_list],
+    #    }).to_csv('doc/stock_predict_%s.csv'%get_dt(datetime.datetime.now()), index=False)
+    #else:
+    #    print "没有满足条件的alphatree"
+
+    #5、构建投资组合
+    max_stock_num = 5
     if len(alphatree_score_list) > 0:
+        stock_list = list()
+        score_list = list()
+        sharp_list = list()
+        day_list = list()
+        weight_list = list()
+        if len(alphatree_score_list) == 1 and len(alphatree_score_list[0].codes) == 1:
+            stock_list.append(alphatree_score_list[0].codes[0])
+            score_list.append(alphatree_score_list[0].score)
+            sharp_list.append(alphatree_score_list[0].score/alphatree_score_list[0].score_stddev)
+            day_list.append(alphatree_score_list[0].hold_day_num)
+            weight_list.append(1.0)
+        else:
+            cur = datetime.datetime.now()
+            dt = "%d-%d-%d"%(cur.year,cur.month,cur.day)
+            msp = MaxSharpePortfolio()
+            for alphatree_score in alphatree_score_list:
+                for code in alphatree_score.codes:
+                    msp.add_stock(code, dataProxy.history(code, dt, 25, '1d','close'), alphatree_score.score / alphatree_score.score_stddev)
+                    stock_list.append(code)
+                    score_list.append(alphatree_score.score)
+                    sharp_list.append(alphatree_score.score / alphatree_score.score_stddev)
+                    day_list.append(alphatree_score.hold_day_num)
+            weight_list = msp.create()
+            sort_index = np.argsort(-weight_list)
+            stock_list = [stock_list[index] for index in sort_index]
+            score_list = [score_list[index] for index in sort_index]
+            sharp_list = [sharp_list[index] for index in sort_index]
+            day_list = [day_list[index] for index in sort_index]
+            weight_list = [weight_list[index] for index in sort_index]
         #存档选择的stock
         pd.DataFrame({
-            'stock': [','.join(codes) for codes in [alphatree_score.codes for alphatree_score in alphatree_score_list]],
-            'score': [alphatree_score.score for alphatree_score in alphatree_score_list],
-            'sharp': [alphatree_score.score / alphatree_score.score_stddev for alphatree_score in alphatree_score_list],
-            'day': [alphatree_score.hold_day_num for alphatree_score in alphatree_score_list],
-        }).to_csv('doc/stock_predict_%s.csv'%get_dt(datetime.datetime.now()), index=False)
+                'stock': stock_list,
+                'score': score_list,
+                'sharp': sharp_list,
+                'day': day_list,
+                'weight': weight_list,
+            }).to_csv('doc/stock_predict_%s.csv'%get_dt(datetime.datetime.now()), index=False)
     else:
         print "没有满足条件的alphatree"
